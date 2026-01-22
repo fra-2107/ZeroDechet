66b9108a553bcf8f1d1f278b7a43e23fcc8d1ddb
fra-2107 <timchimienti@gmail.com>
2026-01-22 17:51:27 +0100

    feat: Ajout de la configuration Nginx et mise à jour des ports dans docker-compose

--- docker/api/server.js
+++ docker/api/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5000', 'http://localhost:8000', 'http://127.0.0.1:8000', 'http://127.0.0.1:5500', 'http://localhost:5500']
+  origin: ['http://localhost:5000', 'http://localhost:8000', 'http://127.0.0.1:8000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:4200']
 }));
 
 app.use(express.json());
