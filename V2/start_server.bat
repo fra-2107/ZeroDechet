070aa1e2cce898bf479a04b970dfa0b07d2f2c72
Raphael Cardinal <raphi.cardi@gmail.com>
2026-01-22 17:41:30 +0100

    Suppression des fichiers obsolètes, y compris les scripts SQL, les fichiers de configuration, et les données JSON, pour nettoyer le projet et réduire la taille du dépôt.

--- V2/start_server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-python -m http.server 8000
-endlocal
--- backforfront/php/constant.php
+++
@@ -1,5 +0,0 @@
-<?php
-  define('DB_USER', 'backlog_user');
-  define('DB_PASSWORD', 'backlog_pass');
-  define('DB_NAME', 'backlog_db');
-  define('DB_SERVER', 'localhost');
